afd7aec20ec31b89b51b18d733bb297eebb69024
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-06-03 17:22:33 +0200

    created css/js assets with npm run dev + removed bootstraps cdn

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
     <!-- Font Google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -19,8 +16,8 @@
     <!-- Swiper -->
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
     
-    <!-- My CSS -->
-    <link rel="stylesheet" href="/css/presto.css">
+    <!-- CSS (mine + bootstrap) -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Presto.it | {{$title ?? ''}}</title>
     <link rel="shortcut icon" href="/media/pLogoNewNew.png" type="image/x-icon">
 </head>
@@ -43,19 +40,18 @@
     <!-- Easter Egg Toast Message -->
     <div id="easterEggToastMessage" class="toast align-items-center text-white back-sec border-0" role="alert" aria-live="assertive" aria-atomic="true">
     </div>
-    
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
     <!-- Swiper Js -->
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
     
-    <!-- My JS -->
-    @if(Route::currentRouteName()=='welcome')
-        <script src="/js/presto.js"></script>
-    @elseif(Route::currentRouteName()=='allAds')
-        <script src="/js/ads.js"></script>
-    @endif        
+    <!-- My JS + Bootstrap's -->
+
+        <!-- Nel building con npm run dev, mi crea problemi se faccio il join dei miei 2 js -->
+        @if(Route::currentRouteName()=='welcome')
+            <script src="/js/presto.js"></script>
+        @elseif(Route::currentRouteName()=='allAds')
+            <script src="/js/ads.js"></script>
+        @endif        
 
 
 </body>
